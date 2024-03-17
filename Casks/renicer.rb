f3b15f0dd32e585271fd6ce42a60989f74eb759c
datwinz <fmdrewes@gmail.com>
2024-03-17 13:48:35 +0100

    Added renicer

---
+++ Casks/renicer.rb
@@ -0,0 +1,15 @@
+cask "renicer" do
+  version "0.1.0"
+  sha256 "6b23a55306fa5a4d64d65734ba4a119d92c1b06364f3bf5b7e6eda9bd6610052"
+
+  url "https://github.com/datwinz/renicer/releases/download/v#{version}/Glance.zip"
+  name "Renicer"
+  desc "App to show nice values of processes and renice them"
+  homepage "https://github.com/datwinz/renicer"
+
+  app "Glance.app"
+
+  zap trash: [
+    "/Users/floor/Library/Saved Application State/com.example.renicer.savedState"
+  ]
+end
