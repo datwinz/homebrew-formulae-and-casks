a628dba8a5f7927abf134030299c7e418564fff5
datwinz <fmdrewes@gmail.com>
2024-03-17 13:52:12 +0100

    Fixed spelling mistake

--- Casks/renicer.rb
+++ Casks/renicer.rb
@@ -2,7 +2,7 @@ cask "renicer" do
   version "0.1.0"
   sha256 "6b23a55306fa5a4d64d65734ba4a119d92c1b06364f3bf5b7e6eda9bd6610052"
 
-  url "https://github.com/datwinz/renicer/releases/download/v#{version}/renicer.zip"
+  url "https://github.com/datwinz/renicer/releases/download/#{version}/renicer.zip"
   name "Renicer"
   desc "App to show nice values of processes and renice them"
   homepage "https://github.com/datwinz/renicer"
