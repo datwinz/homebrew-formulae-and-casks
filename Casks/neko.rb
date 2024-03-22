b1741dd115c967a43fe14407367dc586ef780d1e
datwinz <fmdrewes@gmail.com>
2024-03-22 21:22:18 +0100

    Added neko

---
+++ Casks/neko.rb
@@ -0,0 +1,22 @@
+cask "neko" do
+  version "1.1"
+  sha256 "ed3a93bb988754bc7a5f6af49e5af0b935d6f80190fa2ab1148d7abb9a5f1fd6"
+
+  url "https://github.com/mdonoughe/neko-mac/releases/tag/v#{version}/Neko-#{version}.dmg"
+  name "Neko"
+  desc "Oneko in Cocoa (Neko for Mac OS X)"
+  homepage "https://github.com/mdonoughe/neko-mac"
+
+  zap trash: [
+    "~/Library/Application Scripts/com.yourcompany.neko",
+    "~/Library/Containers/com.yourcompany.neko",
+  ]
+
+  caveats do
+    <<~EOS
+    For neko to work on all desktop spaces, put neko in your dock.
+    After that right-click it and assign it to all spaces.
+    EOS
+  end
+
+end
