98544d90f9a0281379655dd3b434a310c78471fb
datwinz <fmdrewes@gmail.com>
2024-03-22 21:23:58 +0100

    Update neko name

--- Casks/neko.rb
+++ Casks/neko.rb
@@ -3,7 +3,7 @@ cask "neko" do
   sha256 "ed3a93bb988754bc7a5f6af49e5af0b935d6f80190fa2ab1148d7abb9a5f1fd6"
 
   url "https://github.com/mdonoughe/neko-mac/releases/tag/v#{version}/Neko-#{version}.dmg"
-  name "Neko"
+  name "猫"
   desc "Oneko in Cocoa (Neko for Mac OS X)"
   homepage "https://github.com/mdonoughe/neko-mac"
 
