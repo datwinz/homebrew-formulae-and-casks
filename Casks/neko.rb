d87bfcc1cc71482ecb74021512da07c70f1f9eeb
datwinz <fmdrewes@gmail.com>
2024-03-22 21:29:37 +0100

    fix shasum again

--- Casks/neko.rb
+++ Casks/neko.rb
@@ -1,6 +1,6 @@
 cask "neko" do
   version "1.1"
-  sha256 "f42deaf56913ad258075a1309536054189e4fe38a237245fd90648f15502ace2"
+  sha256 "ed3a93bb988754bc7a5f6af49e5af0b935d6f80190fa2ab1148d7abb9a5f1fd6"
 
   url "https://github.com/mdonoughe/neko-mac/releases/download/v#{version}/Neko-#{version}.dmg"
   name "猫"
