99af713cc879bbd19e57b5ba19677e6c4b52f46a
datwinz <fmdrewes@gmail.com>
2025-01-01 16:11:52 +0100

    Fix error

--- Casks/dmenu-mac.rb
+++ Casks/dmenu-mac.rb
@@ -2,7 +2,7 @@ cask "dmenu-mac" do
   version "0.7.3"
   sha256 "b81e75ca07d3977dc88f683e2bb35bd149c170f1e7c430233f6f23d1feca0d01"
 
-  url "https://github.com/datwinz/dmenu-mac/releases/download/#{version}/dmenu-mac.zip"
+  url "https://github.com/datwinz/dmenu-mac/releases/download/v#{version}/dmenu-mac.zip"
   name "dmenu-mac"
   desc "Keyboard-only application launcher"
   homepage "https://github.com/datwinz/dmenu-mac"
