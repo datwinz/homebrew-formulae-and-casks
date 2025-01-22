e255f56945e337d1626cf740965f1b5cde3caa09
datwinz <fmdrewes@gmail.com>
2025-01-22 19:20:43 +0100

    dmenu-mac 0.7.9.1

--- Casks/dmenu-mac.rb
+++ Casks/dmenu-mac.rb
@@ -1,6 +1,6 @@
 cask "dmenu-mac" do
-  version "0.7.9"
-  sha256 "d4032eeeee814928e87543677c19283c13c75e086bb19c7e0472aaa5db854808"
+  version "0.7.9.1"
+  sha256 "34aca35a785eea3797bd7dd3a88235e44e7a4398a614649927137b910a1ef208"
 
   url "https://github.com/datwinz/dmenu-mac/releases/download/v#{version}/dmenu-mac.zip"
   name "dmenu-mac"
