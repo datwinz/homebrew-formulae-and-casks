5eb624d81d800a1b182592e9f5e5dcdb758f6e1f
datwinz <fmdrewes@gmail.com>
2026-02-02 21:27:50 +0100

    Fix audit warnings

--- Casks/docfetcher.rb
+++ Casks/docfetcher.rb
@@ -2,7 +2,8 @@ cask "docfetcher" do
   version "1.1.27"
   sha256 "081061af1925f782307a0f23e05277a7299a650575354cbd6b977ba2a8c550d6"
 
-  url "https://altushost-swe.dl.sourceforge.net/project/docfetcher/docfetcher/#{version}/DocFetcher-#{version}-macOS-64bit-NonPortable.dmg"
+  url "https://downloads.sourceforge.net/docfetcher/DocFetcher-#{version}-macOS-64bit-NonPortable.dmg",
+    verified: "sourceforge.net/docfetcher/"
   name "DocFetcher"
   desc "Open-source desktop search application which allows you to search the contents of files on your computer"
   homepage "https://docfetcher.sourceforge.io/"
