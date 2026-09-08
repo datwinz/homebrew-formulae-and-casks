6d229eb92baec88d5c5cadc76ef4cb53a0ec92ec
datwinz <fmdrewes@gmail.com>
2026-09-08 20:26:42 +0200

    Add mpd

---
+++ Formula/mpd-dt.rb
@@ -0,0 +1,164 @@
+class MpdDt < Formula
+  desc "Music Player Daemon"
+  homepage "https://www.musicpd.org/"
+  url "https://github.com/MusicPlayerDaemon/MPD/archive/refs/tags/v0.24.15.tar.gz"
+  sha256 "448172fcd26aa6eb8bfe95c998cf7bd612ae6beaf5ba54f2cbf984d0249d3de4"
+  license "GPL-2.0-or-later"
+  head "https://github.com/MusicPlayerDaemon/MPD.git", branch: "master"
+
+  bottle do
+    sha256 cellar: :any, arm64_tahoe:   "9ccb678b69385e65b691614700ae8a28b94aa433f950810cb884e41de8533c1e"
+    sha256 cellar: :any, arm64_sequoia: "af59a81ff56c2cb8b33c3db3d71de65e5e76248ee4926b0b9ef3610216db0726"
+    sha256 cellar: :any, arm64_sonoma:  "e8f1eff0ae9283bdf9bfcf7213d876e9eafdd7442388d69ff86d486b58ea64c1"
+    sha256 cellar: :any, sonoma:        "b4f98d8dfc40a0c37ffc541cb9b9dcb1d4ea33069631a3621c7ae5a7c1b9bd68"
+    sha256 cellar: :any, arm64_linux:   "8cc6c4c0c0d958f4564f378ee96738f9858ba386f65a123618ce900bb705d6b4"
+    sha256 cellar: :any, x86_64_linux:  "848f5e47e9e44deb6072e3ae082dbe9b10064ce0d7f9ac5ba62eb7eaab8afb2c"
+  end
+
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+  depends_on "nlohmann-json" => :build
+  depends_on "pkgconf" => :build
+
+  depends_on "chromaprint"
+  depends_on "faad2"
+  depends_on "ffmpeg"
+  depends_on "flac"
+  depends_on "fluid-synth"
+  depends_on "fmt"
+  depends_on "game-music-emu"
+  depends_on "icu4c@78"
+  depends_on "lame"
+  depends_on "libao"
+  depends_on "libid3tag"
+  depends_on "libmikmod"
+  depends_on "libmpdclient"
+  depends_on "libnfs-dt"
+  depends_on "libnpupnp"
+  depends_on "libogg"
+  depends_on "libsamplerate"
+  depends_on "libshout"
+  depends_on "libsndfile"
+  depends_on "libsoxr"
+  depends_on "libvorbis"
+  depends_on "mpg123"
+  depends_on "opus"
+  depends_on "pcre2"
+  depends_on "sqlite"
+  depends_on "wavpack"
+
+  uses_from_macos "bzip2"
+  uses_from_macos "curl"
+  uses_from_macos "expat"
+
+  on_ventura :or_older do
+    depends_on "llvm"
+
+    fails_with :clang do
+      cause "Needs C++20 std::make_unique_for_overwrite"
+    end
+  end
+
+  on_linux do
+    depends_on "systemd" => :build
+    depends_on "alsa-lib"
+    depends_on "dbus"
+    depends_on "jack"
+    depends_on "pipewire"
+    depends_on "pulseaudio"
+    depends_on "systemd"
+    depends_on "zlib-ng-compat"
+  end
+
+  # Work around superenv to avoid mixing `expat` usage in libraries across dependency tree.
+  # Brew `expat` usage in Python has low impact as it isn't loaded unless pyexpat is used.
+  # TODO: Consider adding a DSL for this or change how we handle Python's `expat` dependency
+  def remove_brew_expat
+    env_vars = %w[CMAKE_PREFIX_PATH HOMEBREW_INCLUDE_PATHS HOMEBREW_LIBRARY_PATHS PATH PKG_CONFIG_PATH]
+    ENV.remove env_vars, /(^|:)#{Regexp.escape(formula_opt_prefix("expat"))}[^:]*/
+    ENV.remove "HOMEBREW_DEPENDENCIES", "expat"
+  end
+
+  def install
+    if OS.mac? && MacOS.version <= :ventura
+      remove_brew_expat
+      ENV.append "LDFLAGS", "-L#{formula_opt_lib("llvm")}/unwind -lunwind"
+      # When using Homebrew's superenv shims, we need to use HOMEBREW_LIBRARY_PATHS
+      # rather than LDFLAGS for libc++ in order to correctly link to LLVM's libc++.
+      ENV.prepend_path "HOMEBREW_LIBRARY_PATHS", formula_opt_lib("llvm")/"c++"
+    end
+
+    args = %W[
+      --sysconfdir=#{etc}
+      -Dmad=disabled
+      -Dmpcdec=disabled
+      -Dao=enabled
+      -Dbzip2=enabled
+      -Dchromaprint=enabled
+      -Dexpat=enabled
+      -Dffmpeg=enabled
+      -Dfluidsynth=enabled
+      -Dnfs=enabled
+      -Dshout=enabled
+      -Dupnp=npupnp
+      -Dvorbisenc=enabled
+      -Dwavpack=enabled
+      -Dgme=enabled
+      -Dmikmod=enabled
+      -Dnlohmann_json=enabled
+      -Dsystemd_system_unit_dir=#{lib}/systemd/system
+      -Dsystemd_user_unit_dir=#{lib}/systemd/user
+    ]
+
+    system "meson", "setup", "output/release", *args, *std_meson_args
+    system "meson", "compile", "-C", "output/release", "--verbose"
+    ENV.deparallelize # Directories are created in parallel, so let's not do that
+    system "meson", "install", "-C", "output/release"
+
+    pkgetc.install "doc/mpdconf.example" => "mpd.conf"
+  end
+
+  def caveats
+    <<~EOS
+      MPD requires a config file to start.
+      Please copy it from #{etc}/mpd/mpd.conf into one of these paths:
+        - ~/.mpd/mpd.conf
+        - ~/.mpdconf
+      and tailor it to your needs.
+    EOS
+  end
+
+  service do
+    run [opt_bin/"mpd", "--no-daemon"]
+    keep_alive true
+    process_type :interactive
+    working_dir HOMEBREW_PREFIX
+  end
+
+  test do
+    assert_match "[wavpack] wv", shell_output("#{bin}/mpd --version")
+
+    require "expect"
+
+    port = free_port
+
+    (testpath/"mpd.conf").write <<~EOS
+      bind_to_address "127.0.0.1"
+      port "#{port}"
+    EOS
+
+    plugin = OS.mac? ? "osx" : "pulse"
+
+    io = IO.popen("#{bin}/mpd --stdout --no-daemon #{testpath}/mpd.conf 2>&1", "r")
+    io.expect("output: Successfully detected a #{plugin} audio device", 30)
+
+    ohai "Connect to MPD command (localhost:#{port})"
+    TCPSocket.open("localhost", port) do |sock|
+      assert_match "OK MPD", sock.gets
+      ohai "Ping server"
+      sock.puts("ping")
+      assert_match "OK", sock.gets
+      sock.close
+    end
+  end
+end
